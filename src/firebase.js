7c34218d6501e76496f8be5403e03d00dbe936a9
Gokhan Egri <egrigokhan@gmail.com>
2020-02-01 15:44:05 +0300

    Add files via upload

---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+export const initializeReactApp = () => {
+    const config = {
+        apiKey: "AIzaSyB1-l7M0hUHpMAsmSb0O1lH9Xe1PzLXogk",
+	    authDomain: "pregotest-f2b81.firebaseapp.com",
+	    databaseURL: "https://pregotest-f2b81.firebaseio.com",
+	    projectId: "pregotest-f2b81",
+	    storageBucket: "pregotest-f2b81.appspot.com",
+	    messagingSenderId: "472911257841",
+	    appId: "1:472911257841:web:6ce1359e129777e6570950"
+    };
+
+    firebase.initializeApp(firebaseConfig);
+}
